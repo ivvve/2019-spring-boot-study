ab7f2d21b2031b5d92c63a8624e62afc7d372007
devson <sonyc5720@gmail.com>
2019-01-20 03:44:13 +0900

    log 설정 추가

--- community/src/main/java/com/community/web/controller/BoardController.java
+++ community/src/main/java/com/community/web/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.community.web.controller;
 
 import com.community.web.domain.Board;
 import com.community.web.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("/board")
 public class BoardController {
@@ -30,9 +32,15 @@ public class BoardController {
     }
 
     @GetMapping("/list")
-    public String list(@PageableDefault Pageable pageable, Model model) {
+    public String list(@PageableDefault Pageable pageable, Model model) { // Pageable 필드 디버깅으로 확인
+        log.debug("요청받은 page 정보 : {}", pageable);
+
         Page<Board> boardList = boardService.findBoardList(pageable);
         model.addAttribute("boardList", boardList);
+
+        log.debug("총 element 수 : {}, 전체 page 수 : {}, 페이지에 표시할 element 수 : {}, 현재 페이지 index : {}, 현재 페이지의 element 수 : {}",
+                boardList.getTotalElements(), boardList.getTotalPages(), boardList.getSize(),
+                boardList.getNumber(), boardList.getNumberOfElements());
 
         return "board/list"; // resources/templates/ + board/list + .html
     }
