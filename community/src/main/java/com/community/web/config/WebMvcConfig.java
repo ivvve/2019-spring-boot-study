c98e75c849a489d04025e08244472f1196f9d8e0
devson <sonyc5720@gmail.com>
2019-01-24 12:16:24 +0900

    argument resolver 등록용 WebMvcConfig 클래스 생성

---
+++ community/src/main/java/com/community/web/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.community.web.config;
+
+import com.community.web.resolver.UserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private UserArgumentResolver userArgumentResolver;
+
+    public WebMvcConfig(UserArgumentResolver userArgumentResolver) {
+        this.userArgumentResolver = userArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);
+    }
+}
