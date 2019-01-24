63e6329c16fac685ffd3b10e07d78a96e478c2c7
devson <sonyc5720@gmail.com>
2019-01-24 17:03:42 +0900

    초기 데이터를 위한 commandLineRunner() 생성

--- rest-web/src/main/java/com/community/rest/RestApplication.java
+++ rest-web/src/main/java/com/community/rest/RestApplication.java
@@ -1,7 +1,18 @@
 package com.community.rest;
 
+
+import com.community.rest.domain.Board;
+import com.community.rest.domain.User;
+import com.community.rest.domain.enums.BoardType;
+import com.community.rest.repository.BoardRepository;
+import com.community.rest.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class RestApplication {
@@ -10,5 +21,29 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(UserRepository userRepository, BoardRepository boardRepository) {
+		return arg -> {
+			User chris = userRepository.save(User.builder()
+					.name("Chris")
+					.password("test1234!@")
+					.email("chris@naver.com")
+					.createdDate(LocalDateTime.now())
+					.build());
+
+			// Paging을 위해 200개의 데이터를 넣는다.
+			IntStream.rangeClosed(1, 154).forEach(index ->
+				boardRepository.save(Board.builder()
+						.title("게시글" + index)
+						.subTitle("순서" + index)
+						.content("테스트" + index)
+						.boardType(BoardType.free)
+						.createdDate(LocalDateTime.now())
+						.updatedDate(LocalDateTime.now())
+						.user(chris)
+						.build())
+			);
+		};
+	}
 }
 
