65276003d1519e3613315d773da2c14ac1c5761f
devson <sonyc5720@gmail.com>
2019-01-24 16:15:04 +0900

    community 게시판에 사용되는 entity, enum, repository 생성

---
+++ rest-web/src/main/java/com/community/rest/domain/Board.java
@@ -0,0 +1,41 @@
+package com.community.rest.domain;
+
+import com.community.rest.domain.enums.BoardType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY: 키 생성을 DB에 위임
+    private Long index;
+    private String title;
+    private String subTitle;
+    private String content;
+    @Enumerated(EnumType.STRING) //EnumType.ORDINAL은 enum의 순번으로 저장, String은 해당 enum 그대로 저장
+    private BoardType boardType;
+    // LocalDateTime: 1.8부터 추가된 API로 Date, Calendar에서 부실한 날짜 연산기능을 추가로 제공
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY) // fetch: 가지고오다, EAGER: DB에서 조회 시 바로 User 객체를 가져옴, LAZY: 조회 후 객체를 사용할 시에 가져옴
+    private User user; // 실제 User 객체가 DB에 저장되는 것이 아닌 User의 index가 record에 저장된다.
+
+    @Builder
+    public Board(String title, String subTitle, String content, BoardType boardType, LocalDateTime createdDate, LocalDateTime updatedDate, User user) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.content = content;
+        this.boardType = boardType;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.user = user;
+    }
+}
