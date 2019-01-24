dfd54e89fae1653ecb5c937aeee980738a3aa60f
devson <sonyc5720@gmail.com>
2019-01-24 17:13:35 +0900

    security 관련 설정 추가

---
+++ rest-web/src/main/java/com/community/rest/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.community.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        http.httpBasic()
+                .and()
+                    .authorizeRequests()
+                        .anyRequest().permitAll() // 모든 요청 허용
+                .and()
+                    .cors()
+                        .configurationSource(source)
+                .and()
+                    .csrf()
+                        .disable();
+    }
+}
